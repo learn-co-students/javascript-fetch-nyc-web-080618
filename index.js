9c73b46179b38f1ba9304ef04978b581970dff68
madtab7 <madtab7@gmail.com>
2018-09-24 12:01:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const app = "I don't do much.";
+const token = '53e4879d6747053a906233b31f1c8638abece563'
+fetch('https://api.github.com/madtab7/repos', {
+  headers: {
+    Authorization:`token ${token}`
+  }
+})
+  .then(res => res.json()).
+  .then(json => console.log(json))
